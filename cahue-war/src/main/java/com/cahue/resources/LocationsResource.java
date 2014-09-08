ebaf06cad40c55e23a5e632b1a4e1507c0e14dc9
Francesco <f.gatti@speexx.com>
2014-09-08 18:07:23 +0200

    Incoming positions stored

--- cahue-war/src/main/java/com/cahue/resources/LocationsResource.java
+++ cahue-war/src/main/java/com/cahue/resources/LocationsResource.java
@@ -1,11 +1,11 @@
 package com.cahue.resources;
 
-import com.cahue.DAO;
 import com.cahue.DataSource;
-import com.cahue.TestClass;
 import com.cahue.entities.Location;
+import com.google.appengine.api.search.*;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,8 +16,10 @@ import javax.ws.rs.core.Response;
 @Path("/loc")
 public class LocationsResource {
 
+    private final static String LOCATIONS_INDEX = "locations";
+
     @Inject
-    DAO dao;
+    DataSource dataSource;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -29,7 +31,34 @@ public class LocationsResource {
     @Consumes({MediaType.APPLICATION_JSON})
     @Path("/put")
     public Response put(Location location) {
-        dao.saveLocation(location);
+
+        Double latitude = location.getLatitude();
+        Double longitude = location.getLongitude();
+
+        // Save in datastore
+        EntityManager em = dataSource.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(location);
+        em.getTransaction().commit();
+
+        // Save in Index
+        GeoPoint geoPoint = new GeoPoint(latitude, longitude)  ;
+        String documentId = location.getId().toString();
+        Document doc = Document.newBuilder()
+                .setId(documentId) // Setting the document identifer is optional. If omitted, the search service will create an identifier.
+                .addField(Field.newBuilder().setName("location").setGeoPoint(geoPoint))
+                .build();
+
+        IndexSpec indexSpec = IndexSpec.newBuilder().setName(LOCATIONS_INDEX).build();
+        Index index = SearchServiceFactory.getSearchService().getIndex(indexSpec);
+        try {
+            index.put(doc);
+        } catch (PutException e) {
+            if (StatusCode.TRANSIENT_ERROR.equals(e.getOperationResult().getCode())) {
+                // retry putting the document
+            }
+        }
+
         return Response.status(Response.Status.OK).build();
     }
 }
