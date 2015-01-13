1fe59881866878adec0d04ef65aad51265e42770
Francesco <f.gatti@speexx.com>
2015-01-13 18:15:05 +0100

    User auth service in progress

--- cahue-war/src/main/java/com/cahue/users/UserAuthService.java
+++ cahue-war/src/main/java/com/cahue/users/UserAuthService.java
@@ -1,17 +1,17 @@
 package com.cahue.users;
 
-import com.cahue.util.GoogleAuth;
+import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.services.oauth2.Oauth2;
+import com.google.api.services.oauth2.model.Userinfoplus;
+import com.google.api.services.plus.Plus;
+import com.google.api.services.plus.model.Person;
 
+import javax.servlet.ServletException;
 import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Date: 13.01.15
@@ -20,50 +20,50 @@ import java.util.List;
  */
 public class UserAuthService {
 
-    private final List mClientIDs;
-    private final String mAudience;
-    private final GoogleIdTokenVerifier mVerifier;
-    private final JsonFactory mJFactory;
+    private Credential credential;
 
-    private  Credential credential;
+    public static final void main(String[] args) throws Exception {
+        new UserAuthService("ya29.-gBs2W-JFt0fMdxcZ93TO-5WS1g1GKQAAub4Z0Oqo1irr6Mo6wMhlkg4Du24AAxXJnpSijwv1CUYHQ");
+    }
 
-    private String mProblem = "Verification failed. (Time-out?)";
-
-    public UserAuthService(String[] clientIDs, String audience) {
-        mClientIDs = Arrays.asList(clientIDs);
-        mAudience = audience;
-        NetHttpTransport transport = new NetHttpTransport();
-        mJFactory = new JacksonFactory();
-        mVerifier = new GoogleIdTokenVerifier(transport, mJFactory);
+    public UserAuthService(final String accessToken) {
+//        AuthorizationCodeFlow authFlow = null;
         try {
-            credential = GoogleAuth.authorizeService(transport, mJFactory, Arrays.asList("https://www.googleapis.com/auth/plus.login"));
-        } catch (IOException e) {
+//            authFlow = initializeFlow();
+//            Credential credential = authFlow.loadCredential(getUserId(req));
+            credential = new GoogleCredential.Builder()
+                    .setRequestInitializer((new HttpRequestInitializer() {
+                        @Override
+                        public void initialize(HttpRequest request)
+                                throws IOException {
+                            request.getHeaders().setAuthorization("Bearer " + accessToken);
+                        }
+                    }))
+                    .build();
+
+            Oauth2 oauth2 = new Oauth2.Builder(UserAuthUtils.HTTP_TRANSPORT, UserAuthUtils.JSON_FACTORY, credential.getRequestInitializer()).build();
+
+            Userinfoplus userinfoplus = oauth2.userinfo().get().execute();
+            System.out.println(userinfoplus);
+
+            // Build the Plus object using the credentials
+            Plus plus = new Plus.Builder(
+                    UserAuthUtils.HTTP_TRANSPORT, UserAuthUtils.JSON_FACTORY, credential).setApplicationName("").build();
+            // Make the API call
+            Person profile = plus.people().get("me").execute();
+            System.out.println(profile);
+        }
+//        catch (ServletException e) {
+//            e.printStackTrace();
+//        }
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public GoogleIdToken.Payload check(String tokenString) {
-        GoogleIdToken.Payload payload = null;
-        try {
-            GoogleIdToken token = GoogleIdToken.parse(mJFactory, tokenString);
-            if (mVerifier.verify(token)) {
-                GoogleIdToken.Payload tempPayload = token.getPayload();
-                if (!tempPayload.getAudience().equals(mAudience))
-                    mProblem = "Audience mismatch";
-                else if (!mClientIDs.contains(tempPayload.getAuthorizedParty()))
-                    mProblem = "Client ID mismatch";
-                else
-                    payload = tempPayload;
-            }
-        } catch (GeneralSecurityException e) {
-            mProblem = "Security issue: " + e.getLocalizedMessage();
-        } catch (IOException e) {
-            mProblem = "Network problem: " + e.getLocalizedMessage();
-        }
-        return payload;
+    protected AuthorizationCodeFlow initializeFlow() throws ServletException, IOException {
+        return UserAuthUtils.initializeFlow();
     }
 
-    public String problem() {
-        return mProblem;
-    }
+
 }
